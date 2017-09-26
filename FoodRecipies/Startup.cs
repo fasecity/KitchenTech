9ccea5e5d73a735c42cb447cab9e6cb9efe46010
mo <mohamoud51@hotmail.com>
2017-09-26 19:42:17 -0400

    Add project files.

---
+++ FoodRecipies/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodRecipies.Startup))]
+namespace FoodRecipies
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
